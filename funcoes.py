9d4f559453b7127f345c524512e687160c8caa18
jmanoelinsper <joaompf@al.insper.edu.br>
2021-06-02 10:58:53 -0300

    Funcoes dos itens e da pontuacao

--- funcoes.py
+++ funcoes.py
@@ -1,6 +1,7 @@
 import pygame as pg
 import config
 
+#Carrega as imagens do jogo
 def novo_sprite(image, width, height, x, y):
     fundo_image = pg.image.load(image)
     fundo_sprite = pg.sprite.Sprite()
@@ -13,3 +14,25 @@ def novo_sprite(image, width, height, x, y):
     fundo_sprite.rect.y = y
 
     return fundo_sprite
+
+#Coloca as imagens nas dimensoes do jogo
+def sprite_fundo(imagem):
+    return novo_sprite(imagem, width=config.WIDTH, height=config.HEIGHT, x=0,  y=0)
+
+#Cria os itens do jogo
+def cria_item(item, width, height, x, y):
+    imagem = 'imagens/itens/' + item + '.png'
+    sprite = novo_sprite(imagem, width=width, height=height, x=x, y=y)
+
+    sprite.rect.x -= width / 2
+    sprite.rect.y -= height / 2
+
+    return sprite
+
+#Pontuacao do jogador
+score_font = pg.font.SysFont('consolas', 60, True)
+
+#Score na tela do jogo
+def scoreDisplay(win, score, x, y):
+    score_text = score_font.render(str(score), True, (0, 0, 0))
+    win.blit(score_text, (x, y))
